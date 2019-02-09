5ea4fd90f67ae4103771e6e048daa0af00844efa
Tyler Valley <tvalley93@gmail.com>
2019-02-08 19:54:22 -0500

    Add username to users model and finish sign up page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_08_010937) do
+ActiveRecord::Schema.define(version: 2019_02_08_232922) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "username", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
